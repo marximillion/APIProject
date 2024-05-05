a9ae1b973105775a4a07fa884cea30cd70002255
marximillion <marximillion@gmail.com>
2024-05-04 19:54:54 -0600

    create App skeleton

--- App.tsx
+++ App.tsx
@@ -1,118 +1,59 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
+ * Imports
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { Component, ReactNode } from "react";
+import { Text } from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+/**
+ * Props
+ */
+interface Props {}
+/**
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+ * State
+ */
+interface State {}
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+/**
+ * Root render of the app
+ */
+export default class App extends Component<Props, State> {
+  /**
+   * Constructor
+   * 
+   * @param props
+   */
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }// End of constructor()
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  /**
+   * On Mount
+   */
+  public componentDidMount(): void {
+    console.log('App::OnMount');
+  }// End of componentDidMount()
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  /**
+   * Un Mount
+   */
+  public componentWillUnmount(): void {
+    console.log('App::UnMount');  
+  }// End of componentWillUnmount()
+  
+  /**
+   **********RENDER METHODS********** 
+   */
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+  /**
+   * Main Render
+   */
+  public render(): ReactNode {
+    return (
+      <Text>{'APP'}</Text>
+    );
+  }// End of render()
+}// End of class
+// End of file
