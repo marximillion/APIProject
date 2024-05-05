4aef245133cc7f716d4c5fbe9817327d9bf2a0b7
marximillion <marximillion@gmail.com>
2024-05-04 21:10:46 -0600

    create NavigableAppContainer - WIP, not wired in

---
+++ app/navigation/NavigableAppContainer.tsx
@@ -0,0 +1,33 @@
+/**
+ * Imports
+ */
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StackParamList } from "./StackParamList";
+
+/**
+ * Initialization
+ */
+const Stack = createStackNavigator<StackParamList>();
+
+/**
+ * Main Container
+ * Configure navigation routing and screen stack for the application
+ */
+export default function NavigableAppContainer() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        id={'MainStack::NavigableAppContainer'}
+        initialRouteName={'Home'}
+        screenOptions={{headerShown: false}}
+      >
+        <Stack.Screen
+          name={'Home'}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
