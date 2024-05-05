af932b0595860ea038dcddf475779fd969782801
marximillion <marximillion@gmail.com>
2024-05-04 21:44:44 -0600

    create Movie Agent skeleton

---
+++ app/lib/api/agents/MovieAgent.ts
@@ -0,0 +1,15 @@
+/**
+ * Imports
+ */
+
+/**
+ * Constants
+ */
+const MOVIE_API_KEY = 'a67c9774';
+const MOVIE_URL = 'https://www.omdbapi.com/';
+
+/**
+ * Handles the API for making movie API calls
+ */
+export default class MovieAgent {
+}
