97c05640217c0ef3e02beb7ef35e9fe1538860ed
marximillion <marximillion@gmail.com>
2024-05-04 23:40:52 -0600

    create Weather Agent

--- app/lib/api/agents/MovieAgent.ts
+++ app/lib/api/agents/MovieAgent.ts
@@ -2,6 +2,10 @@
  * Imports
  */
 
+import {APIPayload} from '../../APIPayload';
+import Error from '../models/Error';
+import Movies from '../models/Movies';
+
 /**
  * Constants
  */
@@ -12,4 +16,39 @@ const MOVIE_URL = 'https://www.omdbapi.com/';
  * Handles the API for making movie API calls
  */
 export default class MovieAgent {
-}
+  /**
+   * Make call for movie API
+   */
+  public searchMovies = async () => {
+    console.log('MovieAgent::searchMovies');
+
+    try {
+      // 1: Construct the URL
+      const url = `${MOVIE_URL}?s=game&apikey=${MOVIE_API_KEY}`;
+      console.log(url);
+
+      // 2: Fetch the API
+      const response: Response = await fetch(url);
+
+      // 3: Retrieve the JSON response
+      const jsonResponse: APIPayload = await response.json();
+
+      // 4: Check for OK Response, retrieve message if call failed and throw error to catch
+      if (!response.ok) {
+        throw Error.createFromJSON(jsonResponse);
+      }
+
+      return Movies.createFromJSON(jsonResponse);
+    } catch (error: any) {
+      return error;
+    }
+  }; // End of searcjMovies()
+
+  /**
+   * Make call for movie details API
+   */
+  public getMovieDetails = async () => {
+    console.log('MovieAgent::getMovieDetails');
+  }; // End of getMovieDetails()
+} // End of class
+// End of file
